3c0dc734b70aa07a0cde49e61e2f607eb50b5050
Aditya Mangla <aadimangla@gmail.com>
2021-06-17 23:44:19 +0530

    Route Controllers

--- server.js
+++ server.js
@@ -3,34 +3,28 @@ const app = express();
 const ejs = require("ejs");
 const path = require("path")
 const expressLayout = require("express-ejs-layouts");
-
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
+//Database Connection
+mongoose.connect("mongodb://localhost:27017/cakesDB", { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: true });
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log('Database Connected');
+}).catch(err => {
+    console.log('Connection Failed!');
+});
+
+//Assests
+app.use(express.static('public'));
 
 //Set template engine
 app.use(expressLayout)
 app.set('views', path.join(__dirname, '/resources/views'))
 app.set('view engine', 'ejs')
 
+require('./routes/web.js')(app)
 
-//Assests
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.render('home');
-})
-
-app.get('/cart', (req, res) => {
-    res.render('customers/cart');
-})
-
-app.get('/login', (req, res) => {
-    res.render('auth/login');
-})
-
-app.get('/register', (req, res) => {
-    res.render('auth/register');
-})
 
 
 app.listen(PORT, () => {
